b06a157485aebe6d9962afa3b0ea622b07f925ae
Daniel-Botache <daniel.botache@gmail.com>
2023-11-09 18:11:34 -0500

    se hacen cambios en la ruta

--- server/src/routes/priceRouter.ts
+++ server/src/routes/priceRouter.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { getSpecialPriceHandler } from "../handlers/getSpecialPriceHandler";
 
 const priceRouter: Router = Router();
-
+//esta ruta debe eliminarse
 priceRouter.get("/:user_id/:nombre_producto", getSpecialPriceHandler);
 
 export default priceRouter;
--- server/src/routes/stockRouter.ts
+++ server/src/routes/stockRouter.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { getStockHandler } from "../handlers/getStockHandler";
 
 const stockRouter: Router = Router();
-
+//eliminar esta ruta
 stockRouter.get("/", getStockHandler);
 
 export default stockRouter;
