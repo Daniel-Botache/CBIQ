8dabbd5d7fe3d9af26de3f667d1f7d3940d9e7b1
Daniel-Botache <daniel.botache@gmail.com>
2023-11-10 16:01:26 -0500

    se eliminan carpetas para replantear el proyecto

--- server/src/routes/priceRouter.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getSpecialPriceHandler } from "../handlers/getSpecialPriceHandler";
-
-const priceRouter: Router = Router();
-//esta ruta debe eliminarse
-priceRouter.get("/:user_id/:nombre_producto", getSpecialPriceHandler);
-
-export default priceRouter;
--- server/src/routes/stockRouter.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getStockHandler } from "../handlers/getStockHandler";
-
-const stockRouter: Router = Router();
-//eliminar esta ruta
-stockRouter.get("/", getStockHandler);
-
-export default stockRouter;
