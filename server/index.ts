63498253da3573c7a67752d2669d889a1802d329
Daniel-Botache <daniel.botache@gmail.com>
2023-11-12 23:46:26 -0500

    se agregan variables de entorno

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,15 @@
 import express from "express";
 import morgan from "morgan";
+
+// access to PORT from .env file
+const PORT_ENV: string | undefined = process.env.PORT;
+
 const app = express();
 app.use(express.json);
+app.use(morgan("dev"));
 
-const PORT = 3000;
+//Asigna 3000 como valor por defecto en caso de que PORT_ENV sea undefined
+const PORT = parseInt(PORT_ENV || "3000", 10);
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
