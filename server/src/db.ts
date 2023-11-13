0b125137e96cdaf0ee079b0cff822dc35a17e184
Daniel-Botache <daniel.botache@gmail.com>
2023-11-13 02:03:04 -0500

    se agrega configuracion para la DB en TS

--- server/src/db.ts
+++ server/src/db.ts
@@ -2,9 +2,17 @@ import { Sequelize } from "sequelize";
 import fs from "fs";
 import path from "path";
 
+const DB_BASE: string | undefined = process.env.DB_BASE;
 const DB_USER: string | undefined = process.env.DB_USER;
 const DB_PASSWORD: string | undefined = process.env.DB_PASSWORD;
 const DB_HOST: string | undefined = process.env.DB_HOST;
 
 //New instance of sequalize with de .env data
-const sequelize = new Sequelize(``);
+if (DB_BASE && DB_USER && DB_HOST && DB_PASSWORD) {
+  const sequelize = new Sequelize(DB_BASE, DB_USER, DB_PASSWORD, {
+    host: DB_HOST,
+    dialect: "postgres",
+  });
+} else {
+  throw new Error("Missing database connection details");
+}
