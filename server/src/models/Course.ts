91bc2ba4f80bfd8278df6954f6bd5dba79138fa4
Daniel-Botache <daniel.botache@gmail.com>
2023-11-17 16:04:22 -0500

    se agrega configuracion para el modelo de Course

---
+++ server/src/models/Course.ts
@@ -0,0 +1,36 @@
+import { DataTypes, Sequelize } from "sequelize";
+
+export const Course = (sequelize: Sequelize) => {
+  sequelize.define("Course", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    name: { type: DataTypes.STRING, allowNull: false },
+    price: { type: DataTypes.FLOAT, allowNull: false },
+    timetable: { type: DataTypes.STRING, allowNull: false },
+    dateInit: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    dateEnd: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    intensity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    videoURL: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    imageURL: { type: DataTypes.STRING, allowNull: false },
+    status: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+      defaultValue: false,
+    },
+  });
+};
