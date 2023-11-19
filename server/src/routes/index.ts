2720a1f73472787baff1bd7d7940fb6978d3a529
Daniel-Botache <daniel.botache@gmail.com>
2023-11-18 23:45:15 -0500

    se agrega configuración para el index de rutas de usuario

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
-
+import { userRouter } from "./userRouter/userRoutes";
+//Router Init
 const routes = Router();
+
+//Every route configuration
+routes.use("/user", userRouter);
 
 export { routes };
---
+++ server/src/routes/userRouter/userRoutes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+//router init
+const userRouter = Router();
+
+//Route configuration
+userRouter.get("/");
+
+export { userRouter };
