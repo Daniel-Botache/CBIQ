f88c59d7bf3d791e8f9dcdeb17f442d68599bc2f
Daniel-Botache <daniel.botache@gmail.com>
2023-11-24 19:13:06 -0500

    se soluciona bug response en el controller

--- server/src/controllers/userControllers/userController.ts
+++ server/src/controllers/userControllers/userController.ts
@@ -1,11 +1,12 @@
 import { User } from "../../db";
 import { Request, Response } from "express";
-const getAllUSerController = async (_req: Request, res: Response) => {
+
+const getAllUSerController = async (_req: Request, res: Response<any, any>) => {
   try {
     const users = await User.findAll();
     return res.status(200).send(users);
   } catch (error) {
-    return res(500).json({ error: "Internal server error" });
+    return res.status(500).json({ error: "Internal server error" });
   }
 };
 
