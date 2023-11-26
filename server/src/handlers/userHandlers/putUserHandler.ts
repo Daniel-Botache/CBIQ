349e0d308e1d8ae4253a0b41f7e83a0178aaa54c
Daniel-Botache <daniel.botache@gmail.com>
2023-11-26 18:55:52 -0500

    se hace el codigo para el handler de modificar el usuario

--- server/src/handlers/userHandlers/putUserHandler.ts
+++ server/src/handlers/userHandlers/putUserHandler.ts
@@ -1,28 +1,14 @@
 import { User } from "../../db";
 import { userDataInterface } from "../../helpers/interfaces";
-import {
-  ModelStatic,
-  ModelAttributeColumnOptions,
-  Model,
-  InferAttributes,
-  InferCreationAttributes,
-  CreationOptional,
-  Attributes,
-} from "sequelize";
 
 export const putUserHandler = async (id: string, data: userDataInterface) => {
-  const { name, gender, tel, password, imageUrl } = data;
   try {
     const user = await User.findByPk(id);
-    if (user) {
-      user.name = name;
-      user.gender = gender;
-      user.tel = tel;
-      user.password = password;
-      user.imageUrl = imageUrl;
-      await user.save();
-      return "Modification completed";
+    if (!user) {
+      return "User not found";
     }
+    await user.update(data);
+    return "User modified";
   } catch (error) {
     return "Error trying to modify the user";
   }
