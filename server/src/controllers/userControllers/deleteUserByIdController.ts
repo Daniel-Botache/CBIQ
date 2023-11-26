7ba49de489052662f14c3a854dba0894b1e0cb73
Daniel-Botache <daniel.botache@gmail.com>
2023-11-25 19:20:32 -0500

    se crea el controller para delete del ususario

---
+++ server/src/controllers/userControllers/deleteUserByIdController.ts
@@ -0,0 +1,15 @@
+import { deleteUserByIdHandler } from "../../handlers/userHandlers/deleteUserByIdHandler";
+import { Response, Request } from "express";
+
+export const deleteUserByIdController = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const deleteUser = await deleteUserByIdHandler(id);
+    if (deleteUser) {
+      return res.status(200).send(deleteUser);
+    }
+    return res.status(404).send("User not found");
+  } catch (error) {
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
