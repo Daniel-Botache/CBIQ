940dff38e5d402904e5dce91dbd2f3344585d76e
Daniel-Botache <daniel.botache@gmail.com>
2023-11-26 19:28:19 -0500

    se hace el codigo para las rutas put y post de usuario

--- server/src/routes/userRouter/userRoutes.ts
+++ server/src/routes/userRouter/userRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import getAllUSerController from "../../controllers/userControllers/getAllUserController";
 import { postUserController } from "../../controllers/userControllers/postUserController";
 import { deleteUserByIdController } from "../../controllers/userControllers/deleteUserByIdController";
+import putUSerController from "../../controllers/userControllers/putUserController";
 
 //router init
 const userRouter = Router();
@@ -10,5 +11,6 @@ const userRouter = Router();
 userRouter.get("/", getAllUSerController);
 userRouter.post("/", postUserController);
 userRouter.delete("/:id", deleteUserByIdController);
+userRouter.put("/:id", putUSerController);
 
 export { userRouter };
