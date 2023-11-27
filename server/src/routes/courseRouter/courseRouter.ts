2a0f65540fab6632adbc4b1ee87c2f2a11ce0e09
Daniel-Botache <daniel.botache@gmail.com>
2023-11-27 01:44:37 -0500

    se hace el codigo para la configuracion de rutas de cursos

---
+++ server/src/routes/courseRouter/courseRouter.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+//router init
+const courseRouter = Router();
+
+//Route configuration
+courseRouter.get("/");
+
+export { courseRouter };
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -6,6 +6,7 @@ import { categoryRouter } from "./categoryRouter/categoryRouter";
 import { teacherRouter } from "./teacherRouter/teacherRouter";
 import { reviewRouter } from "./reviewRouter/reviewRouter";
 import { loginRouter } from "./loginRouter/loginRouter";
+import { courseRouter } from "./courseRouter/courseRouter";
 //Router Init
 const routes = Router();
 
@@ -16,6 +17,7 @@ routes.use("/country", countryRouter);
 routes.use("/category", categoryRouter);
 routes.use("/teacher", teacherRouter);
 routes.use("/review", reviewRouter);
+routes.use("/course", courseRouter);
 routes.use("/login", loginRouter);
 
 export { routes };
