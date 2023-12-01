254a13c1861f8bb0593827d88124b385b3c9a42c
Daniel-Botache <daniel.botache@gmail.com>
2023-12-01 11:51:46 -0500

    se hace el la ruta para el handler para putDefault

--- server/src/controllers/putDefaultController.ts
+++ server/src/controllers/putDefaultController.ts
@@ -1,15 +1,23 @@
 import { ModelDefined } from "sequelize";
 import { Request, Response } from "express";
 
-class putController {
+class PutController {
   private model: ModelDefined<any, any>;
   constructor(model: ModelDefined<any, any>) {
     this.model = model;
   }
   async put(req: Request, res: Response) {
     try {
-      const data = req.body;
+      const dataRequest = req.body;
       const { id } = req.params;
-    } catch (error) {}
+      const data = await this.model.findByPk(id);
+      data ? data.update(dataRequest) : res.status(404).send("Data not Found");
+      return res.status(200).send("Data modified");
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send("Internal server error");
+    }
   }
 }
+
+export default PutController;
--- server/src/routes/userRouter/userRoutes.ts
+++ server/src/routes/userRouter/userRoutes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { postUserController } from "../../controllers/userControllers/postUserController";
-import putUSerController from "../../controllers/userControllers/putUserController";
 import GetController from "../../controllers/getAllDefaultController";
 import DeleteController from "../../controllers/deleteDafaultController";
 import GetByIdController from "../../controllers/getByIdDefaultController";
+import PutController from "../../controllers/putDefaultController";
 import { sequelize } from "../../db";
 //router init
 const userRouter = Router();
@@ -11,6 +11,7 @@ const userRouter = Router();
 const userController = new GetController(sequelize.models.User);
 const userDeleteController = new DeleteController(sequelize.models.User);
 const userGetByIdController = new GetByIdController(sequelize.models.User);
+const userPutController = new PutController(sequelize.models.User);
 
 //Route configuration
 userRouter.get("/", userController.getAll.bind(userController));
@@ -19,7 +20,7 @@ userRouter.delete(
   "/:id",
   userDeleteController.delete.bind(userDeleteController)
 );
-userRouter.put("/:id", putUSerController);
+userRouter.put("/:id", userPutController.put.bind(userPutController));
 userRouter.get(
   "/:id",
   userGetByIdController.getById.bind(userGetByIdController)
