342ace84bbea2f620d9f153f078d5c87758c14b0
Daniel Botache <120441453+Daniel-Botache@users.noreply.github.com>
2024-11-19 15:37:14 -0500

    Update postUserHandler.ts

--- server/src/handlers/userHandlers/postUserHandler.ts
+++ server/src/handlers/userHandlers/postUserHandler.ts
@@ -2,6 +2,7 @@ import { User } from "../../db";
 import { userDataInterface } from "../../helpers/interfaces";
 
 export const postUserHandler = async (data: userDataInterface) => {
+  return "Hola mundo";
   const { name, gender, tel, email, password, countFrom, imageUrl } = data;
   const [_user, created] = await User.findOrCreate({
     where: { email },
